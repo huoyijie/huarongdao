8008d7922fa97fc66f29019bef0cdeae77c08d32
huoyijie <yijie.huo@foxmail.com>
2023-11-16 14:31:26 +0800

    add react-DND

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,8 +1,16 @@
 import { CUBE_SIZE, blockBgColor } from "@/lib/defs"
+import { useDrag } from "react-dnd"
 
 export default function Block({ w, h, x, y, hero }) {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'block',
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging()
+    })
+  }))
+
   return (
-    <div className={`w-${w * CUBE_SIZE} h-${h * CUBE_SIZE} left-${x * CUBE_SIZE} top-${y * CUBE_SIZE} absolute p-0.5 hover:cursor-grab`}>
+    <div ref={drag} className={`w-${w * CUBE_SIZE} h-${h * CUBE_SIZE} left-${x * CUBE_SIZE} top-${y * CUBE_SIZE} absolute p-0.5 hover:cursor-grab ${isDragging ? 'opacity-50' : ''}`}>
       <div className={`w-full h-full rounded shadow-lg flex justify-center items-center text-4xl text-white ${blockBgColor(hero)}`}>
         {hero}
       </div>
--- src/components/Huarongdao.jsx
+++ src/components/Huarongdao.jsx
@@ -1,12 +1,16 @@
 import { CUBE_SIZE, WIDTH, HEIGHT } from "@/lib/defs"
 import Block from "./Block"
+import { DndProvider } from "react-dnd"
+import { HTML5Backend } from "react-dnd-html5-backend"
 
 export default function Huarongdao({ blocks }) {
   return (
-    <div className={`w-${WIDTH * CUBE_SIZE} h-${HEIGHT * CUBE_SIZE} bg-gray-200 rounded shadow relative`}>
-      {blocks.map(([w, h, x, y, hero], i) => (
-        <Block key={i} w={w} h={h} x={x} y={y} hero={hero} />
-      ))}
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className={`w-${WIDTH * CUBE_SIZE} h-${HEIGHT * CUBE_SIZE} bg-gray-200 rounded shadow relative`}>
+        {blocks.map(([w, h, x, y, hero], i) => (
+          <Block key={i} w={w} h={h} x={x} y={y} hero={hero} />
+        ))}
+      </div>
+    </DndProvider>
   )
 }
